0f3aa4608068088f2356898990dd897c94aae8c8
Viraj-Sankalpa <virajsankalpa@gmail.com>
2024-05-31 07:50:17 +0530

    logo

--- components/Logo.js
+++ components/Logo.js
@@ -1,10 +1,9 @@
-import React from 'react';
 
 const Logo = () => {
   return (
     <div style={{ textAlign: 'center', margin: '20px 0' }}>
       {/* <img src="/logo.png" alt="Logo" style={{ height: 50 }} /> */}
-      <h1>Book an Appointment for Interview</h1>
+      <h1>Book an Appointment for Interviews</h1>
     </div>
   );
 };
